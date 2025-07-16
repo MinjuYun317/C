956d2ff4606ef2ab385c4fa200df832e55706cb5
MinjuYun317 <mjy19428@gmail.com>
2025-07-16 23:51:22 +0900

    Add files via upload

---
+++ 예제 3-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int income = 0;
+	double tax;
+	const double tax_rate = 0.12;
+
+	income = 456;
+	tax = income * tax_rate;
+	printf("세금은 : %.1lf입니다. \n", tax);
+
+	return 0;
+}
